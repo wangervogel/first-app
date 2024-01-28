99d8a15728358ce28f3a40983fb82b70200383f5
mametaro99 <bearsmichimato99@gmail.com>
2024-01-28 19:10:40 +0900

    Firebaseでログイン・ログアウトの機能を作成しました。

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { signOut } from "firebase/auth";
+import { auth } from "../lib/firebase";
 import { Link } from "react-router-dom";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import '../App.css';
+import { useAuthState } from "react-firebase-hooks/auth"
+import { AppBar, Toolbar, Typography, Button, Box, Avatar } from '@mui/material';
 
 function Header() {
+
+  const [user] = useAuthState(auth)
+
   return (
     <AppBar position="static">
-      <Toolbar>
+      <Toolbar style={{ justifyContent: 'space-between' }}>
         <Link to="/" style={{ color: 'inherit', textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
           <img src="wangel_logo.jpeg" alt="Logo" style={{ width: '50px', height: '50px', marginRight: '20px' }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             装備管理Todo
           </Typography>
         </Link>
+        <Box display="flex" alignItems="center">
+          {user ? (
+            <>
+              <Avatar src={user.photoURL} alt="" sx={{ width: 40, height: 40, marginRight: 1 }} />
+              <Button color="inherit" onClick={() => signOut(auth)}>Logout</Button>
+            </>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">Login</Button>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,8 +4,17 @@ import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../lib/firebase";
 
 const Login = () => {
+
+  const GoogleProvider = new GoogleAuthProvider();
+
+  const signInWithGoogle = async () => {
+      await signInWithPopup(auth, GoogleProvider)
+  }
+
   return (
       <Container maxWidth="sm" sx={{ pt: 5 }}>
           <Paper elevation={3} sx={{ py: 4, px: 2, mt: 10, textAlign: 'center' }}>
@@ -13,11 +22,12 @@ const Login = () => {
                 好きなアカウントでのログインを行なってください
               </Typography>
               <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, alignItems: 'center' }}>
-                  <Button 
-                    variant="outlined" 
-                    sx={{ 
-                      maxWidth: '200px', 
-                      backgroundColor: 'yellow', 
+                  <Button
+                    onClick={signInWithGoogle}
+                    variant="outlined"
+                    sx={{
+                      maxWidth: '200px',
+                      backgroundColor: 'yellow',
                       color: 'black',
                       '&:hover': {
                         backgroundColor: 'lightyellow',
@@ -25,19 +35,6 @@ const Login = () => {
                     }}
                   >
                     Googleでログイン
-                  </Button>
-                  <Button 
-                    variant="contained" 
-                    sx={{ 
-                      maxWidth: '200px', 
-                      backgroundColor: 'black', 
-                      color: 'white',
-                      '&:hover': {
-                        backgroundColor: 'gray',
-                      }
-                    }}
-                  >
-                    GitHubでログイン
                   </Button>
               </Box>
           </Paper>
